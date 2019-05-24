d2ba92b0447892a9a5bfc5cb2ff5be0505fa90af
chenlinghong <chenlinghong@126.com>
2019-05-24 23:19:07 +0800

    war包支持

--- src/main/java/club/chenlinghong/institution/InstitutionApplication.java
+++ src/main/java/club/chenlinghong/institution/InstitutionApplication.java
@@ -3,13 +3,26 @@ package club.chenlinghong.institution;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("club.chenlinghong.institution.repository.dao")
-public class InstitutionApplication {
+public class InstitutionApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(InstitutionApplication.class, args);
+    }
+
+    /**
+     * WAR包支持
+     *
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(InstitutionApplication.class);
     }
 
 }
